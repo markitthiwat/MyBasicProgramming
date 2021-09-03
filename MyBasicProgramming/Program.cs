0b82abb4ccd15a6f192513b5c808e2ac45146ee8
golfdigg <mark.itthiwat@gmail.com>
2021-09-03 07:52:34 +0700

    3

--- MyBasicProgramming/Program.cs
+++ MyBasicProgramming/Program.cs
@@ -6,14 +6,26 @@ namespace MyBasicProgramming
     {
         public static void Main(string[] args) //method
         {
+            /* Print String */
             Console.WriteLine("HELLO EMPLY!!!");  //Print String
 
+            /* True False Check */
             Console.WriteLine(10==8); //True False Check
 
+            /* print with bracket{} */
             Console.WriteLine("EMPLy!!! EMPLY01 = {0} EMPLY02 = {1}","EMPLY","IT'S EMPLY");
             Console.WriteLine("{0} + {1} = {2}",1,1,2);   //print with bracket{}
-             
-            Console.ReadLine(); //enter for close commandf
+
+            /* Data Variable */
+            int x = 10;
+            int y = 20;
+            float a = 1.1f;
+
+            Console.WriteLine("integer: " + (x + y));
+            Console.WriteLine("float: " + a);
+
+            /* ###################################### */
+            Console.ReadLine(); //enter for close command
         }
     }
 }
