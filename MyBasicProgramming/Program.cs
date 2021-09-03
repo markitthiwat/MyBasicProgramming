f786ae3134a285f9c324678965fb1257cd07b79d
golfdigg <mark.itthiwat@gmail.com>
2021-09-03 08:04:02 +0700

    4

--- MyBasicProgramming/Program.cs
+++ MyBasicProgramming/Program.cs
@@ -16,13 +16,27 @@ namespace MyBasicProgramming
             Console.WriteLine("EMPLy!!! EMPLY01 = {0} EMPLY02 = {1}","EMPLY","IT'S EMPLY");
             Console.WriteLine("{0} + {1} = {2}",1,1,2);   //print with bracket{}
 
-            /* Data Variable */
-            int x = 10;
-            int y = 20;
+            /* Data Variable : Math logic */
+            int x = 10, y = 20;
             float a = 1.1f;
+            double b;
+            b = x + y;
+            bool status = false;
+            char al = 'A';
+            string name = "emply!";
 
-            Console.WriteLine("integer: " + (x + y));
+            Console.WriteLine("integer: " + x + "+" + y + "=" + (x + y));
             Console.WriteLine("float: " + a);
+            Console.WriteLine("double: " + b);
+            Console.WriteLine("boolean: " + status);
+            Console.WriteLine("char: " + al);
+            Console.WriteLine("string: " + name);
+            x = 11;
+            y = 22;
+            Console.WriteLine("change integer: " + x + "+" + y + "=" + (x + y));
+
+            /* Constant */
+
 
             /* ###################################### */
             Console.ReadLine(); //enter for close command
