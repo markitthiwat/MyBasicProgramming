7caf27ebf454bb25284e13bf0de24c80869521d4
golfdigg <mark.itthiwat@gmail.com>
2021-09-03 07:33:21 +0700

    2

---
+++ MyBasicProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyBasicProgramming
+{
+    class MainClass //class
+    {
+        public static void Main(string[] args) //method
+        {
+            Console.WriteLine("HELLO EMPLY!!!");  //Print String
+
+            Console.WriteLine(10==8); //True False Check
+
+            Console.WriteLine("EMPLy!!! EMPLY01 = {0} EMPLY02 = {1}","EMPLY","IT'S EMPLY");
+            Console.WriteLine("{0} + {1} = {2}",1,1,2);   //print with bracket{}
+
+            Console.ReadLine(); //enter for close command
+        }
+    }
+}
